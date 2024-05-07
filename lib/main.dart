4e68ec7502d86fcf360339dd1ae56c1b1e2c14f7
raed.dev@gmail.com <raed.dev@gmail.com>
2024-05-07 22:21:26 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:nowa_runtime/nowa_runtime.dart';
+import 'package:flutter/material.dart';
+
+@NowaGenerated()
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+@NowaGenerated()
+class MyApp extends StatelessWidget {
+  @NowaGenerated({'loader': 'auto-constructor'})
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,26 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import package_info_plus
+import path_provider_foundation
+import rive_common
+import url_launcher_macos
+import video_player_avfoundation
+import wakelock_plus
+import webview_universal
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
+  WebviewUniversalPlugin.register(with: registry.registrar(forPlugin: "WebviewUniversalPlugin"))
+}
